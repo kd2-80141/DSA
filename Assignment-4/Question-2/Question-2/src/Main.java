356d1a73b81377be297e4737cad0ed6aae4d8059
Nishant-Maske <nishantmaske8@gmail.com>
2023-11-05 22:55:23 +0530

    Completed

---
+++ Assignment-4/Question-2/Question-2/src/Main.java
@@ -0,0 +1,25 @@
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		
+		list.addFirst(10);
+		list.addFirst(20);
+		list.addFirst(30);
+		list.addFirst(40);
+		
+		list.display();
+		
+		list.delFirst();
+		
+		list.display();
+		
+		list.delLast();
+		
+		list.display();
+	}
+
+}
